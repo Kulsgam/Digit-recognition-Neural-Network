fafff51bec2b580fabb431656947d9eb07f71096
Kulsgam <kuls.gam@gmail.com>
2024-06-25 15:27:47 +1000

    Gradient calculation

--- src/nn.py
+++ src/nn.py
@@ -4,6 +4,7 @@ from data import ModelData
 from node import xavier_initialization, sigmoid, sigmoid_derivative, z, ActivationType
 
 LAYER_DIMENSIONS = [0, 16, 16, 10]
+LEARNING_RATE = 0.01  # Used so that the model doesn't overshoot the minimum, so fewer amount of backpropogation passes are needed
 
 
 class NeuralNetwork:
@@ -29,9 +30,6 @@ class NeuralNetwork:
     def _activation_derivative(self, x: ActivationType):
         return sigmoid_derivative(x)
 
-    def _error(self, output: ActivationType, target: ActivationType) -> np.float64:
-        return np.sum((output - target) ** 2)  # type: ignore
-
     def _get_layer_activations(
         self,
         prev_layer: np.ndarray[Tuple[int], np.dtype[np.float64]],
@@ -52,6 +50,8 @@ class NeuralNetwork:
         # Initially I train using the entire dataset instead of SGD or mini-batch
         length = self.training_data.images.shape[0]
 
+        # TODO: Recursively call this and multiple backpropogation iterations
+
         for i in range(length):
             image = self.training_data.images[i]
             label = self.training_data.labels[i]
@@ -60,7 +60,53 @@ class NeuralNetwork:
 
             expected = np.zeros(LAYER_DIMENSIONS[-1])
             expected[label] = 1.0
-            error = self._error(self.layers[-1], expected)
+
+            # d_error/d_weight = d_z/d_weight * d_activation/d_z * d_error/d_activation
+            # d_error/d_bias = d_z/d_bias * d_activation/d_z * d_error/d_activation
+
+            # d_z/d_weight = a_prev
+            # d_z/d_bias = 1
+            # d_activation/d_z = sigmoid_derivative(z)
+            # d_error/d_activation = 2 * (output - target)
+
+            # Imagine the -2 layer has y nodes and the -1 layer has x nodes
+
+            d_z_weights = np.atleast_2d(self.layers[-2])  # Shape: (y, 1)
+            d_z_biases = 1
+            d_activation_z = self._activation_derivative(self.layers[-1])  # Shape: (x,)
+            d_error_activation = 2 * (self.layers[-1] - expected)  # Shape: (x,)
+
+            delta = d_activation_z * d_error_activation  # Shape: (x,)
+            delta_2D = np.atleast_2d(delta)  # Shape: (x, 1)
+
+            d_error_weights = np.dot(d_z_weights, delta_2D.T)  # Shape: (y, x)
+            d_error_biases = d_z_biases * delta  # Shape: (x,)
+
+            # Calculating the gradient of the error of the previous layer (d_error/d_activation)
+            # This is so that it can be used to calculate d_error/d_weight
+
+            d_prev_error_activation = np.dot(self.all_weights[-1], delta)  # Shape: (y,)
+            # Now use this to recursively calculate the gradient of the error of the previous layer
+            # What I mean is to repeat
+
+            # The gradient gives the steepest increase so you negate it to reach the minimum
+            # Update weights and biases
+            self.all_weights[-1] -= LEARNING_RATE * d_error_weights
+            self.all_biases[-1] -= LEARNING_RATE * d_error_biases
+
+            # Iterate over all of the weights and biases
+            # Calculate the gradient for each weight and bias using the error
+            # Update the weight and bias using the gradient
+
+            # You can use numpy instead of iterating it all over (just simply think of it as one)
+            # For example the derivative of z_l with respect to w_l is a_(l-1)
+        pass
+
+    def _backpropogate(
+        self,
+        d_error_activation: np.ndarray[Tuple[int], np.dtype[np.float64]],
+        curr_layer_idx: int,
+    ):
         pass
 
     def test(self, testing_data: ModelData) -> float:
